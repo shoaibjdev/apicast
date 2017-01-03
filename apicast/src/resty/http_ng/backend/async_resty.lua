623aab65b85d8c362d9651219f609e4a58a61e54
Michal Cichra <michal@cichra.cz>
2017-01-03 20:00:19 +0100

    [http_ng] import apitools http_ng client
    https://github.com/APItools/monitor/pull/10
    
    APItools had quite advanced http client with custom backends
    and async requests. I hope to take that as a base to create smart
    http client that would understand caching and authentication.
    
    But also having custom backends would make it much easier to test and
    stub responses.

---
+++ apicast/src/resty/http_ng/backend/async_resty.lua
@@ -0,0 +1,85 @@
+local _M = {}
+local response = require 'resty.http_ng.response'
+local http = require 'resty.http'
+
+_M.async = function(req)
+  local httpc = http.new()
+
+  local parsed_uri = assert(httpc:parse_uri(req.url))
+
+  local scheme, host, port, path = unpack(parsed_uri)
+  if not req.path then req.path = path end
+
+  if #req.path == 0 then req.path = '/' end
+
+  local ok, err = httpc:connect(host, port)
+
+  if not ok then
+    print('error connecting ', host, ':', port, ' : ', err)
+    return response.error(err, req)
+  end
+
+  if scheme == 'https' then
+    local verify = req.options and req.options.ssl and req.options.ssl.verify
+    if type(verify) == 'nil' then verify = true end
+
+    local session, err = httpc:ssl_handshake(false, host, verify)
+
+    if not session then
+      return response.error(err, req)
+    end
+  end
+
+  local res, err = httpc:request(req)
+
+  if res then
+    return response.new(res.status, res.headers, function() return (res:read_body()) end)
+  else
+    return response.error(err, req)
+  end
+end
+
+local function future(thread)
+  local ok, res
+
+  local function load(table)
+    if not ok and not res then
+      ok, res = ngx.thread.wait(thread)
+
+      rawset(table, 'ok', ok)
+
+      if not ok then res = response.error(res) end
+
+      for k,v in pairs(res) do
+        if k == 'headers' then
+          rawset(table, k, response.headers.new(v))
+        else
+          rawset(table, k, v)
+        end
+      end
+    end
+  end
+
+  return setmetatable({}, {
+    __len = function(table)
+      load(table)
+      return rawlen(table)
+    end,
+    __pairs = function(table)
+      load(table)
+      rawset(table, 'body', res.body)
+      return next, table, nil
+    end,
+    __index = function (table, key)
+      load(table)
+      return res[key]
+    end
+  })
+end
+
+_M.send = function(request)
+  local thread = ngx.thread.spawn(_M.async, request)
+  return future(thread)
+end
+
+return _M
